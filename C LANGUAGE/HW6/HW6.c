d9d35f4fb100f5b4bc446a2d8942db966a0fd28f
sernanic100 <sernnic100@gmail.com>
2019-03-20 14:36:08 -0400

    few improvements were done to HW6

--- C LANGUAGE/HW6/HW6.c
+++ C LANGUAGE/HW6/HW6.c
@@ -12,7 +12,7 @@ void DisplayArray(int mainArray[], int length);
 int main(){
 
 	printf("nicolas serna\n" );
-	printf("Welcome to the guessong game\n" );
+	printf("Welcome to the guessing game\n" );
 	printf("You have 7 attempts to guess a random number from 1 to 100\n");
 
 	guessGame();
@@ -23,8 +23,11 @@ void guessGame(){
 
 	int sevenGuessesArray [Array_Size];
 	int randomInt = getRandomInteger();
-	int numberGuesses = 0,enteredGuess = 0;
-	
+	//numberGuesses keeps track how many guesses aare made by user
+	int numberGuesses = 0;
+	//enteredGuess keeps track of number entered by user
+	int enteredGuess = 0;
+
 	while(numberGuesses < Array_Size){
 		printf("Enter your next guess\n");
 		scanf("%d",&enteredGuess);
@@ -32,30 +35,34 @@ void guessGame(){
 		//if the user guesses the random int
 		if(enteredGuess == randomInt){
 			printf("Congratulations, you have entered the correct number in %d guess\n",numberGuesses+1 );
+			printf("The correct number was %d\n",randomInt);
 			break;
 		}
+		//if guess is too low
 		else if (enteredGuess < randomInt){
 			printf("Your guess is too low\n" );
 		}
+		//if guess is too high
 		else{
 			printf("Your guess is too high\n");
 		}
 		++numberGuesses;
-		
+
 	}//End of the while loop
+	//if the user failed to guess the number
 	if(numberGuesses == Array_Size){
-			printf("The coorect number was %d\n",randomInt);
-			printf("Unfortunately, you were not able to guess the correct number within 7 guesses\n ",randomInt);
-			
+			printf("The correct number was %d\n",randomInt);
+			printf("Unfortunately, you were not able to guess the correct number within 7 guesses\n ");
+
 		}
-	
+
 	DisplayArray(sevenGuessesArray,numberGuesses);
 }
 // getRandomInteger will return a random integer number between 1 and 100
-// do not make any changes to this functionint 
+// do not make any changes to this functionint
 int getRandomInteger(){
 	static int initialized = 0;
-	if (initialized == 0){ 
+	if (initialized == 0){
 		// if the first time through then
 		srand((unsigned)time(0));  // Initialize random number generator.
 		initialized = 1;
@@ -76,5 +83,5 @@ void DisplayArray(int mainArray [],int length){
 		printf("%d ",number );
 		count++;
 	}
+	printf("\n");
 }
-
